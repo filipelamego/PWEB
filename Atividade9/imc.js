31098f40e92bf5ea13bf8b52c2398d7319604d04
filipelamego <filipeslamego@gmail.com>
2024-04-17 21:35:46 -0300

    Upando atividade 9 no git

---
+++ Atividade9/imc.js
@@ -0,0 +1,34 @@
+function calcularIMC() {
+    var peso = parseFloat(document.getElementById("peso").value);
+    var altura = parseFloat(document.getElementById("altura").value);
+    var imc = 0;
+    var classif = "";
+
+    imc = peso / (Math.pow(altura, 2));
+
+    if (imc < 18.5) {
+        classif = "MAGREZA";
+    }
+    else if (imc >= 18.5 && imc <= 24.9) {
+        classif = "NORMAL";
+    }
+    else if (imc >= 25 && imc <= 29.9) {
+        classif = "SOBREPESO";
+    }
+    else if (imc >= 30 && imc <= 39.9) {
+        classif = "OBESIDADE"
+    }
+    else {
+        classif = "OBESIDADE";
+    }
+
+    document.getElementById("resultado").value = parseFloat(imc.toFixed(2));
+    document.getElementById("classif").value = classif;
+}
+
+function limpar() {
+    document.getElementById("peso").value = "";
+    document.getElementById("altura").value = "";
+    document.getElementById("resultado").value = "";
+    document.getElementById("classif").value = "";
+}
